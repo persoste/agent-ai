ba1690c97a1a7f14c4d38ec1f322a3758fbd261f
persoste <b123e567@gmail.com>
2025-06-06 21:40:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 
 AOS.init();
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-      entry.target.classList.remove('hide');
-    } else {
-      entry.target.classList.remove('show');
-      entry.target.classList.add('hide');
-    }
+document.querySelectorAll('.faq-item button').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const content = btn.nextElementSibling;
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
   });
-}, {
-  threshold: 0.3
 });
 
-document.querySelectorAll('.section').forEach(el => {
-  el.classList.add('hide');
-  observer.observe(el);
+gsap.registerPlugin(ScrollTrigger);
+
+document.querySelectorAll(".section").forEach(section => {
+  gsap.fromTo(section, 
+    { opacity: 0, y: 100 }, 
+    {
+      scrollTrigger: {
+        trigger: section,
+        start: "top 85%",
+        end: "top 20%",
+        toggleActions: "play reverse play reverse"
+      },
+      opacity: 1,
+      y: 0,
+      duration: 1.2,
+      ease: "power3.out"
+    });
 });
