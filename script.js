27b1e2797db0913367aef6a927447c2fca5f9a35
persoste <b123e567@gmail.com>
2025-06-06 02:04:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-
-document.querySelectorAll('.faq-item button').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const content = btn.nextElementSibling;
-    content.style.display = content.style.display === 'block' ? 'none' : 'block';
-  });
-});
-AOS.init();
+placeholder js
