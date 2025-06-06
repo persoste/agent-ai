20cfc42491b796cbf18985f3ee081665ba55f96f
persoste <b123e567@gmail.com>
2025-06-06 23:03:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
-// SplitText animation demo (testo principale)
-gsap.from(".split-text", {
-  y: 60,
-  opacity: 0,
-  duration: 1.2,
-  ease: "power3.out",
-  delay: 0.2
-});
-
-// Sezioni animate in scroll
-gsap.utils.toArray(".section").forEach(section => {
+gsap.utils.toArray('.section').forEach(section => {
   gsap.from(section, {
+    opacity: 0,
+    y: 100,
+    duration: 0.8,
     scrollTrigger: {
       trigger: section,
       start: "top 80%",
-      toggleActions: "play none none none"
-    },
-    y: 60,
-    opacity: 0,
-    duration: 1.3,
-    ease: "power3.out"
+    }
   });
 });
