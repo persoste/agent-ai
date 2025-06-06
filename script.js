7aa3da94625cb3d8589b95423bedef2548c3d0c4
persoste <b123e567@gmail.com>
2025-06-06 23:09:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 
 gsap.registerPlugin(ScrollTrigger);
-
 gsap.utils.toArray('.section').forEach(section => {
   gsap.from(section, {
     opacity: 0,
     y: 100,
-    duration: 0.8,
+    duration: 1,
     scrollTrigger: {
       trigger: section,
       start: "top 80%",
     }
+  });
+});
+document.querySelectorAll('.faq-question').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const content = btn.nextElementSibling;
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
   });
 });
